74fa85eb1e44d4b13d7d0a2d701efc24da8f080d
Facundo Lewis <facu.lewis@gmail.com>
2020-04-27 11:23:25 -0300

    Added collapsable menu

--- src/layouts/OrganizationView.vue
+++ src/layouts/OrganizationView.vue
@@ -3,6 +3,11 @@
         <a-layout-header class="header">
             <div class="logo">
                 <router-link to="/">Donate</router-link>
+                <a-icon
+                    class="trigger"
+                    :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+                    @click="() => (collapsed = !collapsed)"
+                />
             </div>
             <a-menu
                 theme="dark"
@@ -14,7 +19,15 @@
             </a-menu>
         </a-layout-header>
         <a-layout>
-            <a-layout-sider width="200" style="background: #fff">
+            <a-layout-sider 
+                breakpoint="lg"
+                collapsedWidth="0"
+                collapsible 
+                v-model="collapsed"
+                @collapse="onCollapse"
+                @breakpoint="onBreakpoint"
+                :trigger="null"
+            >
                 <a-menu
                     mode="inline"
                     :defaultSelectedKeys="['/admin']"
@@ -32,7 +45,7 @@
             </a-layout-sider>
             <a-layout style="padding: 24px">        
                 <a-layout-content
-                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+                    :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
                 >
                     <router-view></router-view>
                 </a-layout-content>
@@ -52,13 +65,39 @@ export default {
         ]
     };
   },
+  methods: {
+    onCollapse(collapsed, type) {
+      console.log(collapsed, type);
+    },
+    onBreakpoint(broken) {
+      console.log(broken);
+    },
+  },
 };
 </script>
 
 <style lang="less">
     #main-layout {
+        .header {
+            padding: 0 20px;
+            .logo {
+                width: auto;
+            }
+        }
         .section-title {
             display: inline;
+        }
+        .trigger {
+            color: #fff;
+            font-size: 18px;
+            line-height: 64px;
+            margin-left: 10px;
+            cursor: pointer;
+            transition: color 0.3s;
+        }
+
+        .trigger:hover {
+            color: #1890ff;
         }
     }
 </style>
